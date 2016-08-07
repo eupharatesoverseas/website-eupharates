4215c82a808932f11205f6e86c50102486c56e78
sohaib <ahmadsohaib1216@gmail.com>
2016-08-07 21:21:09 +0100

    version2

--- mySite/overseasWebApp/views.py
+++ mySite/overseasWebApp/views.py
@@ -4,6 +4,11 @@ from django.shortcuts import render
 # from django.http import HttpResponse
 
 def index(request):
-	return render(request,'overseasWebApp/home.html')
+    return render(request,'overseasWebApp/home.html')
+
+
+def about(request):
+    return render(request,'overseasWebApp/about.html')
+
 
 # Create your views here.
